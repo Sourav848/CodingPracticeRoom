d9c5c59f5209ee40545787244f9f2ef296c0a9d4
Kumar <SK076758@cerner.net>
2020-09-29 17:57:58 +0530

    arrayList user input

--- src/main/java/com/sourav/collections/problems/SimpleArrayListUserInput.java
+++ src/main/java/com/sourav/collections/problems/SimpleArrayListUserInput.java
@@ -1,23 +1,24 @@
 package com.sourav.collections.problems;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class SimpleArrayListUserInput {
 
-        public static void main(String args[]){
-            List l=new ArrayList();
-            System.out.println("Enter the input");
-            Scanner input=new Scanner(System.in);
+	public static void main(String args[]) {
+		List l = new ArrayList();
+		System.out.println("Enter the input");
+		Scanner input = new Scanner(System.in);
 
-             String a =input.nextLine();
-             l.add(a);
-   // use this to iterate the value inside the arraylist.
-  /* for (int i = 0; i < l.size(); i++) {
-      System.out.println(l.get(i));
-          } */
-                System.out.println(l);
+		String a = input.nextLine();
+		l.add(a);
+		// use this to iterate the value inside the arraylist.
+		/*
+		 * for (int i = 0; i < l.size(); i++) { System.out.println(l.get(i)); }
+		 */
+		System.out.println(l);
 
-        }
+	}
 
-    }
+}
