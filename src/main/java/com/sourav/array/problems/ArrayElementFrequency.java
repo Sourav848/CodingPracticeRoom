3810e4b8d9cd1e4e16da8ef6cc556b1fa279d8c4
Kumar <SK076758@cerner.net>
2020-10-03 12:55:44 +0530

    new array program added

--- src/main/java/com/sourav/array/problems/ArrayElementFrequency.java
+++ src/main/java/com/sourav/array/problems/ArrayElementFrequency.java
@@ -1,25 +1,56 @@
 package com.sourav.array.problems;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class ArrayElementFrequency {
-	
+
 	public static void main(String args[]) {
 
 		Scanner input = new Scanner(System.in);
 		System.out.print("Enter no. of elements you want in array:");
+		int count = 1;
+		int inc = 0;
+		int inc1 = 0;
 		int n = input.nextInt();
 		int arr[] = new int[n];
+		/* String strArray[] = new String[3]; */
+		int frr[] = new int[n];
+		int prr[] = new int[n];
 		for (int i = 0; i < n; i++) {
 			arr[i] = input.nextInt();
 		}
-		bubblesort(arr);
-		
-	}
-	
-  static void bubblesort(int arr[]) {
+		 bubblesort(arr, n); 
+		/* Arrays.sort(arr); */
+		for (int i = 0; i < arr.length-1; i++) {
+		if (arr[i] == arr[i+1]) 
+			count++;
+		else {
+		   frr[inc++] = count;
+		   prr[inc1++] = arr[i];
+		   count = 1;
+		  }
+		}
+		   frr[inc++] = count;
+		   prr[inc1++] = arr[n-1];
+		   count = 1;
+		for (int i = 0; i < frr.length; i++) {
+			if (frr[i] != 0) 
+			 System.out.println(prr[i]+"   |    "+frr[i]);
+		}
 		
 	}
 
-
+	static void bubblesort(int arr[], int n) {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (arr[i] < arr[j]) {
+					int temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+	}
 }
