5f067636e1e0da7300110e1469ab9a1392c01f16
Kumar <SK076758@cerner.net>
2020-10-03 20:05:00 +0530

    Programs added in array

---
+++ src/main/java/com/sourav/array/problems/MaxMinNumberArray.java
@@ -0,0 +1,24 @@
+package com.sourav.array.problems;
+
+import java.util.Scanner;
+
+public class MaxMinNumberArray {
+	public static void main(String[] args) {  
+        Scanner input = new Scanner(System.in);
+        System.out.println("Define length of array in number"); 
+        int n = input.nextInt();
+        int [] arr = new int[n];  
+        for (int i = 0; i < n; i++) {  
+        	arr[i] = input.nextInt();
+        } 
+        int max = arr[0];
+        int min = arr[0];
+        for (int i = 0; i < n; i++) {  
+        	if(arr[i] > max)
+        		max = arr[i];
+        	else
+        		min = arr[i];
+        }
+        System.out.println("maximum number in array is: "+max+" minimum number in array is: "+min); 
+	}
+}
---
+++ src/main/java/com/sourav/array/problems/ReverseArray.java
@@ -0,0 +1,24 @@
+package com.sourav.array.problems;
+
+import java.util.Scanner;
+
+public class ReverseArray {  
+    public static void main(String[] args) {  
+        Scanner input = new Scanner(System.in);
+        System.out.println("Define length of array in number"); 
+        int n = input.nextInt();
+        int [] arr = new int[n];  
+        for (int i = 0; i < n; i++) {  
+        	arr[i] = input.nextInt();
+        } 
+        System.out.println("Original array: ");  
+        for (int i = 0; i < arr.length; i++) {  
+            System.out.print(arr[i] + " ");  
+        } 
+        System.out.println("Array in reverse order: ");  
+        //Loop through the array in reverse order  
+        for (int i = arr.length-1; i >= 0; i--) {  
+            System.out.print(arr[i] + " ");
+        }  
+    }  
+}  
