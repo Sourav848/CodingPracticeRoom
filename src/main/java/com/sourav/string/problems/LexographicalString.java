06566bab17cf5fb32116b1b7a8db09313943e1ce
Kumar <SK076758@cerner.net>
2020-11-02 01:23:22 +0530

    string programs added

---
+++ src/main/java/com/sourav/string/problems/LexographicalString.java
@@ -0,0 +1,36 @@
+package com.sourav.string.problems;
+
+
+public class LexographicalString {
+public static void main(String[] args)
+    {
+        String str1 = "This is Exercise 1";
+        String str2 = "This is Exercise 4";
+        
+        System.out.println("String 1: " + str1);
+        System.out.println("String 2: " + str2); 
+       
+        // Compare the two strings.
+        int result = str1.compareTo(str2);
+        System.out.println(result);
+        // Display the results of the comparison.
+        if (result < 0)
+        {
+            System.out.println("\"" + str1 + "\"" +
+                " is less than " +
+                "\"" + str2 + "\"");
+        }
+        else if (result == 0)
+        {
+            System.out.println("\"" + str1 + "\"" +
+                " is equal to " +
+                "\"" + str2 + "\"");
+        }
+        else // if (result > 0)
+        {
+            System.out.println("\"" + str1 + "\"" +
+                " is greater than " +
+                "\"" + str2 + "\"");
+        }
+    }
+}
---
+++ src/main/java/com/sourav/string/problems/RemoveWhiteSpace.java
@@ -0,0 +1,15 @@
+package com.sourav.string.problems;
+
+public class RemoveWhiteSpace {
+	
+	 public static void main(String[] args) {    
+         
+	        String str1="Remove white spaces";    
+	            
+	        //Removes the white spaces using regex    
+	        str1 = str1.replaceAll("\\s+", "");   //string, string 
+	            
+	        System.out.println("String after removing all the white spaces : " + str1);    
+	    }    
+
+}
