a583228d3022f24f89596bcb1715d2152d3c0fbb
Akanksha Raj <akanksha.raj@sapiens.com>
2024-09-30 14:56:28 +0530

    Akanksha Test commit

--- src/main/java/com/sourav/basic/problems/ArmstrongNumber.java
+++ src/main/java/com/sourav/basic/problems/ArmstrongNumber.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class ArmstrongNumber {
 
 	public static void main(String args[]) {
-
+//Akanksha Practice
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter a number");
 		int i = 1;
